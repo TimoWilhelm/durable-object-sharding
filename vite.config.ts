783e82560a7572b4f5bda9523be7fbd87a14562a
Timo Wilhelm <mail@timowilhelm.com>
2025-05-17 02:38:37 +0200

    wip sample page

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,31 @@
 import { defineConfig, Plugin } from 'vite';
 import { cloudflare } from '@cloudflare/vite-plugin';
 import path from 'path';
+import { readFile } from 'node:fs/promises';
+
+function importAsText(match: RegExp): Plugin {
+	return {
+		name: 'import-as-text',
+		enforce: 'pre',
+		async load(id) {
+			const path = id.split('?')[0];
+			if (!match.test(path)) return null;
+			return {
+				code: `export default ${JSON.stringify(await readFile(path, 'utf8')).replace(
+					/[\u2028\u2029]/g,
+					(c) => `\\u${`000${c.charCodeAt(0).toString(16)}`.slice(-4)}`
+				)};`,
+				map: { mappings: '' },
+			};
+		},
+	};
+}
 
 export default defineConfig({
-	plugins: [cloudflare()],
+	plugins: [importAsText(/\.sql$/i), cloudflare()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			loader: {
-				'.sql': 'text',
-			},
 		},
 	},
 });
